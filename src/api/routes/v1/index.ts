975d546ccf2b67214d2efe245f7a856200d3a7bf
WasathAtEC <wasath.t@elysiancrest.com>
2023-10-05 23:47:59 +0530

    refactor: add new file-service route

--- src/api/routes/v1/index.ts
+++ src/api/routes/v1/index.ts
@@ -1,9 +1,15 @@
-import express, {Request, Response} from "express";
+import express, { Request, Response } from "express";
+import feedbackRouter from "./feedback.route";
+import fileServiceRouter from "./fileUpload.route";
 
 const router = express.Router();
 
+// http://localhost:3000/api/v1
 router.get("/", (_req: Request, res: Response) => {
-    res.send("Hello from backend!");
-})
+  res.send("Hello from backend!");
+});
+
+router.use("/feedback", feedbackRouter);
+router.use("/file-service", fileServiceRouter);
 
 export default router;
