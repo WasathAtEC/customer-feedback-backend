7d9e4faa59049533e30eb53d42338d398316c960
WasathAtEC <wasath.t@elysiancrest.com>
2023-10-05 23:49:59 +0530

    feat: feedback database model

---
+++ src/models/feedback.model.ts
@@ -0,0 +1,50 @@
+import { IssueCategories } from "./../enums/issueCategories";
+import mongoose from "mongoose";
+
+const feedbackScheema = new mongoose.Schema({
+  company: {
+    type: String,
+    required: [true, "Company is required!"],
+  },
+
+  issueCategory: {
+    type: String,
+    enum: Object.values(IssueCategories),
+    required: [true, "Issue category is required!"],
+  },
+
+  name: {
+    type: String,
+    required: [true, "Name is required!"],
+  },
+
+  email: {
+    type: String,
+    match: /^\S+@\S+\.\S+$/,
+    required: true,
+    unique: false,
+    trim: true,
+    lowercase: true,
+  },
+
+  subject: {
+    type: String,
+    required: [true, "Subject is required!"],
+  },
+
+  message: {
+    type: String,
+    required: [true, "Message is required!"],
+  },
+
+  imageUrl: {
+    type: String,
+  },
+  
+  ticket: {
+    type: String,
+    required: [true, "Ticket generation failed!"],
+  },
+});
+
+export default mongoose.model("Feedback", feedbackScheema);
