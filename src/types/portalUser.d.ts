db1c33bd83dd4432c1b9a6396d3077231ab75dd6
WasathAtEC <wasath.t@elysiancrest.com>
2023-10-06 20:43:49 +0530

    refactor: some type updates

--- src/types/portalUser.d.ts
+++ src/types/portalUser.d.ts
@@ -1,11 +1,26 @@
-export interface IPortalUser {
+export interface ITransformedPortalUser extends IPortalUserInputDTO {
     _id: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export interface IPortalUserInputDTO {
     email: string;
     password: string;
     firstName: string;
     lastName: string;
     company: string;
     role: string;
-    createdAt: Date;
-    updatedAt: Date;
+}
+
+export interface ILoginData {
+    email: string;
+    password: string;
+} 
+
+export interface IJwtToken {
+    email: string;
+    userId: string;
+    role: string;
+    company: string;
 }
