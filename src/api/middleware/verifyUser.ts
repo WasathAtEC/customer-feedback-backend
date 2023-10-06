08e2c2d5179dc9919ac533ca3b587b22e8645f43
WasathAtEC <wasath.t@elysiancrest.com>
2023-10-06 20:41:59 +0530

    feat: verify user middlware (future use)

---
+++ src/api/middleware/verifyUser.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from "express";
+import UserModel from "../../models/user.model";
+
+export const verifyUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { email } = req.method == "GET" ? req.query : req.body;
+
+    let exits = await UserModel.findOne({ email });
+    if (!exits) return res.status(404).send({ error: "Can't find the User" });
+    next();
+  } catch (error) {
+    return res.status(404).send({ message: "Authentication Failed!" });
+  }
+  return 1;
+};
