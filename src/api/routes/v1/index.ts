ce31d51ed9e125dbdd36e537ba47abb94a7d1b54
WasathAtEC <wasath.t@elysiancrest.com>
2023-10-06 20:42:21 +0530

    Add new user routes

--- src/api/routes/v1/index.ts
+++ src/api/routes/v1/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import feedbackRouter from "./feedback.route";
 import fileServiceRouter from "./fileUpload.route";
+import userRouter from "./user.route";
 
 const router = express.Router();
 
@@ -11,5 +12,6 @@ router.get("/", (_req: Request, res: Response) => {
 
 router.use("/feedback", feedbackRouter);
 router.use("/file-service", fileServiceRouter);
+router.use('/user', userRouter)
 
 export default router;
