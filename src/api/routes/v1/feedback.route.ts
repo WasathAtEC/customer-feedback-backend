137e1293d2cfcab3d9376367674d71fe579b1af2
WasathTheekshana <wasath.vt@gmail.com>
2023-10-09 15:57:12 +0530

    feat: plug new routes

--- src/api/routes/v1/feedback.route.ts
+++ src/api/routes/v1/feedback.route.ts
@@ -1,10 +1,24 @@
 import express from "express";
-import { test, createFeedback } from "../../controller/feedback.controller";
+import {
+  test,
+  createFeedback,
+  getAllFeedbacks,
+  getFilteredFeedbacksByIssueCategory,
+  replyFeedback,
+} from "../../controller/feedback.controller";
+import { Auth } from "../../middleware/auth";
 
 const router = express.Router();
 
 // http://localhost:3000/api/v1/feedback/test
 router.get("/test", test);
 router.post("/create-feedback", createFeedback);
+router.get("/get-all-feedbacks", Auth, getAllFeedbacks);
+router.get(
+  "/get-feedbacks-by-issue/:issueCategory",
+  Auth,
+  getFilteredFeedbacksByIssueCategory
+);
+router.post("/reply-feedback/:id", Auth, replyFeedback);
 
 export default router;
