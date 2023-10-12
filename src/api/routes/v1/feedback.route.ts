df166aa72e319d2dd651f61a83cd4fd8f125dd44
WasathTheekshana <wasath.vt@gmail.com>
2023-10-12 11:53:50 +0530

    refactor: unwanted routes delete

--- src/api/routes/v1/feedback.route.ts
+++ src/api/routes/v1/feedback.route.ts
@@ -3,11 +3,9 @@ import {
   test,
   createFeedback,
   getAllFeedbacks,
-  getFilteredFeedbacksByIssueCategory,
   replyFeedback,
   getFeedbackById,
-  getFilteredFeedbacksByCompany,
-  getFilteredFeedbacksByCompanyAndIssueCategory
+  getFilteredFeedbacksByCompanyAndIssueCategory,
 } from "../../controller/feedback.controller";
 import { Auth } from "../../middleware/auth";
 
@@ -16,15 +14,13 @@ const router = express.Router();
 // http://localhost:3000/api/v1/feedback/test
 router.get("/test", test);
 router.post("/create-feedback", createFeedback);
-router.get("/get-all-feedbacks", Auth, getAllFeedbacks);
-router.get("/get-feedback-by-id/:id", Auth, getFeedbackById)
-router.get("/get-feedbacks-by-company/:companyName", Auth, getFilteredFeedbacksByCompany)
-router.get("/get-feedbacks-by-company-and-issue-category/:companyName/:issueCategory", Auth, getFilteredFeedbacksByCompanyAndIssueCategory)
-router.get(
-  "/get-feedbacks-by-issue/:issueCategory",
-  Auth,
-  getFilteredFeedbacksByIssueCategory
-);
 router.post("/reply-feedback/:id", Auth, replyFeedback);
+router.get("/get-all-feedbacks", Auth, getAllFeedbacks);
+router.get(
+  "/get-feedbacks-by-company-and-issue-category",
+  Auth,
+  getFilteredFeedbacksByCompanyAndIssueCategory
+);
+router.get("/get-feedback-by-id/:id", Auth, getFeedbackById);
 
 export default router;
